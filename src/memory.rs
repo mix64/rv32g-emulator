2bd8ed9da2dcd97e4e99e1a986dbd9f056b43911
mix64 <mic@mix64.com>
2020-09-04 10:56:01 +0900

    add 64-bit memory read/write

--- src/memory.rs
+++ src/memory.rs
@@ -34,6 +34,18 @@ impl Memory {
             | ((self.ram[index + 3] as u32) << 24))
     }
 
+    pub fn read64(&self, addr: u32) -> Result<u64, Exception> {
+        let index = addr as usize;
+        Ok((self.ram[index] as u64)
+            | ((self.ram[index + 1] as u64) << 8)
+            | ((self.ram[index + 2] as u64) << 16)
+            | ((self.ram[index + 3] as u64) << 24)
+            | ((self.ram[index + 4] as u64) << 32)
+            | ((self.ram[index + 5] as u64) << 40)
+            | ((self.ram[index + 6] as u64) << 48)
+            | ((self.ram[index + 7] as u64) << 56))
+    }
+
     pub fn write8(&mut self, addr: u32, val: u32) -> Result<(), Exception> {
         let index = addr as usize;
         self.ram[index] = val as u8;
@@ -53,6 +65,18 @@ impl Memory {
         self.ram[index + 1] = ((val >> 8) & 0xff) as u8;
         self.ram[index + 2] = ((val >> 16) & 0xff) as u8;
         self.ram[index + 3] = ((val >> 24) & 0xff) as u8;
+        Ok(())
+    }
+    pub fn write64(&mut self, addr: u32, val: u64) -> Result<(), Exception> {
+        let index = addr as usize;
+        self.ram[index] = (val & 0xff) as u8;
+        self.ram[index + 1] = ((val >> 8) & 0xff) as u8;
+        self.ram[index + 2] = ((val >> 16) & 0xff) as u8;
+        self.ram[index + 3] = ((val >> 24) & 0xff) as u8;
+        self.ram[index + 4] = ((val >> 32) & 0xff) as u8;
+        self.ram[index + 5] = ((val >> 40) & 0xff) as u8;
+        self.ram[index + 6] = ((val >> 48) & 0xff) as u8;
+        self.ram[index + 7] = ((val >> 56) & 0xff) as u8;
         Ok(())
     }
 }
